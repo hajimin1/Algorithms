eb82f2c136c0adf0fe5483f20a798d2b9f18964c
Jimin Ha <smjmjh26@naver.com>
2023-07-29 11:32:21 +0900

    first commit

---
+++ 2775.c
@@ -0,0 +1,34 @@
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//
+//int main(void) {
+//	int k, n, t;
+//	// 층과 호수를 동적 할당
+//	int** ap = (int**)calloc(sizeof(int*), 15);
+//	for (int i = 0; i < 15; i++) {
+//		ap[i] = (int*)calloc(sizeof(int), 15);
+//	}
+//
+//	// 0층에 있는 것은 i호에는 i명
+//	for (int j = 0; j < 15; j++) {
+//		ap[0][j] = j;
+//	}
+//
+//
+//
+//	for (int x = 1; x < 15; x++) {
+//		for (int y = 1; y < 15; y++) {
+//			ap[x][y] = ap[x - 1][y] + ap[x][y - 1];
+//		}
+//	}
+//
+//	scanf_s("%d", &t);
+//	for (int m = 0; m < t; m++) {
+//		scanf_s("%d", &k);
+//		scanf_s("%d", &n);
+//
+//		printf("%d\n", ap[k][n]);
+//	}
+//	
+//}
---
+++ 27866.c
@@ -0,0 +1,11 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//
+//int main(void) {
+//	char S[1000];
+//	int i;
+//	scanf("%s", &S); // scanf_s로 했을 시 자꾸 예외발생
+//	scanf("%d", &i);
+//
+//	printf("%c", S[i - 1]);
+//}
